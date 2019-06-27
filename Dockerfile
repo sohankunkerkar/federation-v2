bafc11c00a825f80967fcfb6257863b32ec76e68
Sohan Kunkerkar <sohank2602@gmail.com>
2019-06-27 16:35:40 -0400

    Initial changes for managed-e2e tests

--- Dockerfile
+++ Dockerfile
@@ -19,7 +19,7 @@ COPY pkg pkg
 COPY cmd cmd
 COPY test test
 COPY vendor vendor
-
+RUN DOCKER_GOPATH_BUILD="/bin/sh -c " make e2e
 RUN DOCKER_BUILD="/bin/sh -c " make hyperfed
 
 # build stage 2:
@@ -27,9 +27,10 @@ FROM registry.svc.ci.openshift.org/openshift/origin-v4.0:base
 
 ENV USER_ID=1001
 
-# copy in binaries
+# copy in binaries 
 WORKDIR /root/
-COPY --from=builder /go/src/sigs.k8s.io/kubefed/bin/hyperfed-linux /root/hyperfed
+COPY --from=builder /go/src/sigs.k8s.io/kubefed/bin/e2e-linux e2e
+COPY --from=builder /go/src/sigs.k8s.io/kubefed/bin/hyperfed-linux hyperfed
 RUN ln -s hyperfed controller-manager && ln -s hyperfed kubefedctl &&  ln -s hyperfed webhook
 
 # user directive - this image does not require root
---
+++ openshift/populate-kubefed-gopath.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eu
+
+# This script populates the federation-v2 directory within a gopath directory
+# structure with the vendored federation-v2 source from this repo. It can be run
+# from any directory. The gopath directory structure is assumed to be rooted at
+# '/go' but can be overriden by setting the GOPATH_DIR environment variable.
+
+dir=$(realpath "$(dirname "${BASH_SOURCE}")/..")
+gopath_dir="${GOPATH_DIR:-"/go"}"
+
+kubefed_gp=github.com/sigs.k8s.io/kubefed
+kubefed_src="${gopath_dir}/src/${kubefed_gp}"
+
+echo "Populating kubefed gopath at ${kubefed_src}"
+mkdir -p $kubefed_src
+cp  Makefile $kubefed_src/Makefile
+cp -r pkg $kubefed_src/pkg
+cp -r cmd $kubefed_src/cmd
+cp -r test $kubefed_src/test
+cp -r hack $kubefed_src/hack
+cp -r scripts $kubefed_src/scripts
+cp -r config $kubefed_src/config
+cp -r vendor $kubefed_src/vendor
